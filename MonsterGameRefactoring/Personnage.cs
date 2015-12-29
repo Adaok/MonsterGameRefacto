64d10d10f038684101291eb81a64fc071562d133
Marco L <marco.loiodice@atos.net>
2015-12-29 12:02:59 +0100

    Implémentation des fonctions et propriétés à utiliser pour l'héritage dans la classe Personnage (Objet Mère de tous)

--- MonsterGameRefactoring/Personnage.cs
+++ MonsterGameRefactoring/Personnage.cs
@@ -8,5 +8,24 @@ namespace MonsterGameRefactoring
 {
     public class Personnage
     {
+
+        public virtual void Attaque(Personnage cible)
+        {
+            
+        }
+
+        public virtual void SubitDegats(int degats)
+        {
+            
+        }
+
+        public virtual bool EstVivant { get; }
+
+        public virtual int LanceLeDe()
+        {
+            Random random = new Random();
+            return random.Next(1, 7);
+        }
+
     }
 }
